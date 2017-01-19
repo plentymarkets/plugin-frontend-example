53d3e15c2b3b492d25d8eea12cc17b536a1d59ba
Christian Kunze <christian.kunze@plentymarkets.com>
2017-01-19 16:28:19 +0100

    Migration Category creation added

--- src/Migrations/PluginFrontendExampleCreateDbMigration_1_0_0.php
+++ src/Migrations/PluginFrontendExampleCreateDbMigration_1_0_0.php
@@ -8,6 +8,9 @@
 
 namespace PluginFrontendExample\Migrations;
 
+use Plenty\Plugin\ConfigRepository;
+use Plenty\Modules\Category\Contracts\CategoryRepositoryContract;
+use Plenty\Modules\Category\Models\Category;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
 use PluginFrontendExample\Models\PluginFrontendExampleDonationAccount;
 use PluginFrontendExample\Models\PluginFrontendExampleDonationTransactions;
@@ -16,12 +19,71 @@ class PluginFrontendExampleCreateDbMigration_1_0_0
 {
 
     /**
+     * @var CategoryRepositoryContract
+     */
+    private $categoryRepo;
+
+    /**
+     * @var ConfigRepository $config
+     */
+    private $config;
+
+    /**
+     * @var array
+     */
+    private $categoriesToCreate = [ 'global.category.home' => [ 'de' => 'Spende',
+                                                                'en' => 'Donation' ]    ];
+
+    /**
+     * CategoriesMigration constructor.
+     * @param CategoryRepositoryContract $categoryRepo
+     */
+    public function __construct(CategoryRepositoryContract $categoryRepo, ConfigRepository $configRepo)
+    {
+        $this->categoryRepo = $categoryRepo;
+        $this->config       = $configRepo;
+    }
+
+    /**
      * @param Migrate $migrate
      */
     public function run(Migrate $migrate)
     {
         $migrate->createTable(PluginFrontendExampleDonationAccount::class);
-
         $migrate->createTable(PluginFrontendExampleDonationTransactions::class);
+
+        $this->createCategory();
+
     }
+
+    private function createCategory()
+    {
+        foreach($this->categoriesToCreate as $configKey => $categories)
+        {
+            $details = [];
+            foreach ($categories as $lang => $name)
+            {
+                $details[] = [
+                    'plentyId' => 0,
+                    'lang'     => $lang,
+                    'name'     => $name
+                ];
+            }
+
+            $newCategory = [ 'parentCategoryId' => '',
+                             'level'            => 0,
+                             'type'             => 'item',
+                             'linklist'         => 'Y',
+                             'right'            => 'all',
+                             'sitemap'          => 'Y',
+                             'details'          => $details,
+                             'clients'          => [ [ 'plentyId' => 0 ] ]
+            ];
+
+            /** @var Category $createdCategory */
+            $createdCategory = $this->categoryRepo->createCategory($newCategory);
+
+        }
+    }
+
 }
